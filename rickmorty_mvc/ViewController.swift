7662cd6b45be3450808c508a510cf7599ae3ed15
Rechimon, Andrés <andres.r1995@hotmail.com>
2023-12-01 11:33:50 -0300

    Initial Commit

---
+++ rickmorty_mvc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rickmorty_mvc
+//
+//  Created by Administrador on 01/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ rickmorty_mvcUITests/rickmorty_mvcUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  rickmorty_mvcUITestsLaunchTests.swift
+//  rickmorty_mvcUITests
+//
+//  Created by Administrador on 01/12/2023.
+//
+
+import XCTest
+
+final class rickmorty_mvcUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
